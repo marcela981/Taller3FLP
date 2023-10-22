f51ba6590105dbaa679dc2afe66199d7317acd91
marcela981 <marcela.mazo@correounivalle.edu.co>
2023-10-22 00:48:40 -0500

    Actualización de código

--- interpretador.rkt
+++ interpretador.rkt
@@ -7,7 +7,7 @@
 
 ;Especificación Léxica
 
-(define scanner-spec-simple-interpreter
+(define scanner-especificacion-lexica
 '(
   (espacio-blanco (whitespace) skip)
   (comentario ("//" (arbno (not #\newline whitespace))) skip)
@@ -20,9 +20,6 @@
   (texto ((arbno letter)) string)
  )
 )
-
-
-;Parte 1 del código diseño de interpretador a la gramática dada.
 
 ;Gramatica
 
@@ -40,7 +37,11 @@
     (expresion (primitiva-unaria "("expresion")") primapp-un-exp)
     (expresion ("Si" expresion "entonces" expresion "sino" expresion "finSI") if-exp)
     (expresion ("declarar" "("  (separated-list identificador "=" expresion ";")   ")" "{" expresion"}") variableLocal-exp)
+    (expresion ("procedimiento" "("(separated-list identificador",")")" "haga" expresion "finProc") procedimiento-exp)
     (expresion ("evaluar" expresion "("(separated-list expresion ",")")" "finEval") app-exp)
+    (expresion ("declaraRec" (arbno identificador "(" (separated-list identificador ",") ")" "=" expresion)  "{" expresion "}") 
+                letrec-exp)
+
 
     
     ;Primitiva binaria
@@ -60,35 +61,37 @@
     )
   )
 
+;//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 ;Parser, Scanner, Interfaz
 
 ;Tipos de datos para la sintaxis abstracta ((Análisis léxico y sintáctico)
 (define scan&parse
-  (sllgen:make-string-parser scanner-spec-simple-interpreter grammar-simple-interpreter))
+  (sllgen:make-string-parser scanner-especificacion-lexica interprerador-gramatica))
 
 ;El Analizador Léxico (Scanner)
 
 (define just-scan
-  (sllgen:make-string-scanner scanner-spec-simple-interpreter grammar-simple-interpreter))
+  (sllgen:make-string-scanner scanner-especificacion-lexica interprerador-gramatica))
   
-(sllgen:make-define-datatypes scanner-spec-simple-interpreter grammar-simple-interpreter)
+(sllgen:make-define-datatypes scanner-especificacion-lexica interprerador-gramatica)
 
 (define show-the-datatypes
-  (lambda () (sllgen:list-define-datatypes scanner-spec-simple-interpreter grammar-simple-interpreter)))
-
+  (lambda () (sllgen:list-define-datatypes scanner-especificacion-lexica interprerador-gramatica)))
 ;El Interpretador (FrontEnd + Evaluación + señal para lectura )
 
 (define interpretador
   (sllgen:make-rep-loop  "--> "
     (lambda (pgm) (eval-program  pgm)) 
     (sllgen:make-stream-parser 
-      scanner-spec-simple-interpreter
-      grammar-simple-interpreter)))
+      scanner-especificacion-lexica
+      interprerador-gramatica)))
 
+;//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-;Parte2
-;Evaluar el programa
+;El interprete
+
+;Función que evalua el programa
 
 
 ;Evalua el ambiente inicial
@@ -118,20 +121,59 @@
 
 ;
 ;Evalua la expresión en el ambiente de entrada
-(define eval-expression
+(define evaluar-expresion
   (lambda (exp env)
     (cases expresion exp
-      (numero-lit (numero) numero)
+      (numero-lit (datum) datum)
+      (texto-lit (txt) txt)
       (var-exp (id) (buscar-variable env id))
-      (else eopl:error '"No existe dentro de la gramática"))))
+      (primapp-un-exp (prim rand)
+                   (apply-primitive-un prim (evaluar-expresion rand env)))
+      (primapp-bin-exp (rand1 prim rand2)
+                   (apply-primitive-bin prim (evaluar-expresion rand1 env) (evaluar-expresion rand2 env))
+                   )
+      (if-exp (test-exp true-exp false-exp)
+              (if (valor-verdad? (evaluar-expresion test-exp env))
+                  (evaluar-expresion true-exp env)
+                  (evaluar-expresion false-exp env)))                   
+      (variableLocal-exp (ids rands body)
+               (let ((args (eval-rands rands env)))
+                 (evaluar-expresion body
+                                  (extended-env ids args env))))
+       ;;Error de definición(?)
+      )))
 
-;Retorna la posicion en la que el caracter esta ubicado
+
+
+;apply-primitive-bin: <primitiva> <expresion> <expresion> -> numero | string
+(define apply-primitive-bin
+  (lambda (prim args1 args2)
+    (cases primitiva-binaria prim
+      (primitiva-suma () (+ args1 args2))
+      (primitiva-resta () (- args1 args2))
+      (primitiva-multi () (* args1 args2))
+      (primitiva-div () (/ args1 args2))
+      (primitiva-concat () (string-append args1 args2))
+      )))
+
+;apply-primitive-un: <primitiva> <expresion> <expresion> -> numero
+(define apply-primitive-un
+  (lambda (prim args)
+    (cases primitiva-unaria prim
+      (primitiva-add1 () (+ args 1))
+      (primitiva-sub1 () (- args 1))
+      )))
+
+
+;/////////////////////////////////////////////////////////Funciones Auxiliares/////////////////////////////////////////////////////////
+
+
+;Función que retorna la posicion en la que el caracter esta ubicado
 (define indice-lista
   (lambda (pred lst)
     (indice-lista-aux 0 pred lst)))
 
-; Función auxiliar que realiza el llamado recursivo utilizando como criterio principal
-; un contador auxiliar que ayuda a determinar el recorrido dentro de la lista
+; Función auxiliar que realiza el llamado recursivo, usando un contador auxiliar que ayuda a determinar el recorrido de la lista
 (define indice-lista-aux
   (lambda (n pred lst)
     (if (null? lst)
@@ -141,25 +183,86 @@
             (indice-lista-aux (+ n 1) pred (cdr lst))))))
 
 ;buscar-pos
-;Funcion auxiliar que retorna el elemento que está ubicado en la posicion dada
-;(define (buscar-pos lst n)
-;  (if (zero? n)
-;     (car lst)
-;      (buscar-pos (cdr lst) (- n 1))))
+;Funcion auxiliar que retorna la posición del lemento
+(define (buscar-pos lst n)
+  (if (zero? n)
+      (car lst)
+      (buscar-pos (cdr lst) (- n 1))))
 
-;función que busca un símbolo en un ambiente dado
-;(define buscar-variable
-;  (lambda (env sym)
-;    (cases environment env
-;      ;No se encontró la variable buscada
-;      (empty-env ()
-;                   (eopl:error '"Error, la variable no existe"))
-      ;busque en la lista de variables si esta la requerida
-;     (extended-env (syms vals env)
-;                           (let ((pos (indice-lista (lambda (x) (eqv? x sym)) syms)))
-;                             (if (number? pos)
-;                                 ; en caso si, trae la posicion, y busque esa posicion en la lista de valores que le corresponde
-;                                 (buscar-pos vals pos)
-                                 ; en caso no, busque de nuevo quitando esa parte ya buscada
 
-;                                 (buscar-variable env sym)))))))
+;/////////////////////////////////////////////////////////Ambientes/////////////////////////////////////////////////////////
+
+
+;****Gramatica*******
+;<env-exp> ::= (empty-env)
+;          ::= (extend-env <list-of symbol>
+;                          <list-of scheme-value> <env-exp>)
+
+;Representación
+(define-datatype environment environment?
+   (empty-env)
+   (extended-env (syms (list-of symbol?))
+                        (vals (list-of scheme-value?))
+                        (env environment?))
+   (recursively-extended-env-record
+                        (proc-names (list-of symbol?))
+                        (idss (list-of (list-of symbol?)))
+                        (bodies (list-of expresion?))
+                        (env environment?))
+  )
+
+(define scheme-value? (lambda (v) #t))
+
+
+;Función que crea un ambiente (Recursivo))
+(define extend-env-recursively
+  (lambda (proc-names idss bodies old-env)
+    (recursively-extended-env-record
+     proc-names idss bodies old-env)))
+
+;función que busca un símbolo
+(define buscar-variable
+  (lambda (env sym)
+    (cases environment env
+      ;No se encontró la variable buscada
+      (empty-env ()
+                   (eopl:error '"Error, la variable no existe"))
+      ;En la lista de variables busca si esta la que se requiere
+      (extended-env (syms vals env)
+                           (let ((pos (indice-lista (lambda (x) (eqv? x sym)) syms)))
+                             (if (number? pos)
+                                 ;Sí está, trae la posicion, y busca esa posicion en la lista de valores que le corresponde
+                                 (buscar-pos vals pos)
+                                 ;No está, busca de nuevo quitando la parte ya buscada
+                                 (buscar-variable env sym))))
+      (recursively-extended-env-record (proc-names idss bodies old-env)
+                                       (let ((pos (list-find-position sym proc-names)))
+                                         (if (number? pos)
+                                             (cerradura (list-ref idss pos)
+                                                      (list-ref bodies pos)
+                                                      env)
+                                             (buscar-variable old-env sym))))
+      )))
+ 
+                                 
+;Función para probar booleanos
+(define valor-verdad?
+  (lambda(x)
+    (not (zero? x))))
+
+;eval-rands
+;Evalua los operandos y los convierte en un ambiente
+(define eval-rands
+  (lambda (rands env)
+    (map (lambda (x) (eval-rand x env)) rands)))
+
+;eval-rand
+;Ingresa el operando, lo llama para evaluar la expresion
+(define eval-rand
+  (lambda (rand env)
+    (evaluar-expresion rand env)))
+
+;/////////////////////////////////////////////////////////Funciones Auxiliares/////////////////////////////////////////////////////////
+
+;Crear  funcion auxiliares para encontrar la posición de un símbolo
+;
